41357bb6f4480e684b8bbd44a052572fdf944bb1
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-04-14 14:58:26 -0300

    refactor: Refactor app.js

--- mongoose-mongodb/src/app.js
+++ mongoose-mongodb/src/app.js
@@ -2,13 +2,13 @@ import { exportPokemonReports } from "./services/pokemonService.js";
 
 const fieldMap = {
   Nome: "Name",
-  "National Dex #": "National Dex #",
-  "Primary Typing": "Primary Typing",
-  "Secondary Typing": "Secondary Typing",
-  Generation: "Generation",
-  "Base Stat Total": "Base Stat Total",
-  "Evolution Stage": "Evolution Stage",
-  "Number of Evolutions": "Number of Evolution",
+  "Número na Pokédex": "National Dex #",
+  "Tipo Primário": "Primary Typing",
+  "Tipo Secundário": "Secondary Typing",
+  Geração: "Generation",
+  "Total de Pontos Base": "Base Stat Total",
+  "Estágio de Evolução": "Evolution Stage",
+  "Quantidade de Evoluções": "Number of Evolution",
 };
 
 exportPokemonReports(fieldMap, "pokemon_reports");
